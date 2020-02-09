288b12c94152c5a2f1e121690b77530833e71ecf
Shawn Cicoria <github@cicoria.com>
2020-02-09 14:07:16 -0500

    hw1 almost done

--- hw/functions.R
+++ hw/functions.R
@@ -1,2 +1,9 @@
 
 `%nin%` = Negate(`%in%`)
+
+
+#paste(c("Hours and Minutes:", max_time %/% 60 , ":", round(max_time %% 60)), collapse = "")
+
+to_hours_minutes <- function (t) {
+  paste(c("Hours and Minutes:", max_time %/% 60 , ":", round(max_time %% 60)), collapse = "")
+}
