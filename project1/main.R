d26bc4be2c748eff69bd1feeb5806984234797a2
Shawn Cicoria <github@cicoria.com>
2020-03-14 21:28:58 -0400

    scraping start

--- project1/main.R
+++ project1/main.R
@@ -1,5 +1,8 @@
 library(logging)
-setLevel("DEBUG")
+getLogger()
+
+library(httr)
+library(xml2)
 
 # functions that based upon a YEAR parameter -- retrieves
 # articles from a pre-determined website.
@@ -8,43 +11,73 @@ setLevel("DEBUG")
 # published in/after that year from your selected journal.
 #
 # As astartpoint, you are required  to  extract  the
-# following 9fieldsfor each article:
+# following 9 fields for each article:
 # Title,  Authors,  Author  Affiliations, Correspondence
 # Author, Correspondence Author's Email, Publish Date,
 # Abstract, Keywords, Full Paper (Text format)
-
-
-# https://www.gmb.org.br
-# years available.
-
-# 1998 - 2019
-
-# root of site: https://www.gmb.org.br/copia-online-version
-
 
 # https://www.ncbi.nlm.nih.gov/pmc/journals/1440/
   #http://xpather.com/
 
 #  http://xpather.com/HLuFhCgv
-
 #  //*[contains(concat( " ", @class, " " ), concat( " ", "issues-cell", " " ))]
 
 ## better
-
 # //*[contains(concat( " ", @class, " " ), concat( " ", "iss-cell", " " ))]
 
 
-
-
 get_articles <- function(start_year = 2019) {
-
-  logdebug(paste("year is: ", start_year))
 
   if (start_year < 1998) {
     stop('invalid year - must be 1998 - 2019')
   }
 
+  main_results <- parse_main_page()
+  issues <- find_all_issue(main_results)
+
+  parse_all_issues(issues)
+
+
   return(0)
+}
+
+parse_all_issues <- function(issues) {
+
+  for(i in 1:49) {# length(issues)) {
+
+    ele <- issues[[i]][["a"]]
+    print(ele[[4]])
+    print(attr(ele, 'href'))
+    #ele <- issues[[i]]$a[[4]] 
+    #print()
+    #attr(d, 'href')
+  }
+
+}
+
+
+find_all_issue <- function(xml_thing) {
+  #expecting -- this is output from parse_main_page below
+  #> class(ro)
+  #[1] "xml_document" "xml_node"
+
+  rv <- xml_find_all(xml_thing, '//*[contains(concat( " ", @class, " " ), concat( " ", "iss-cell", " " ))]')
+
+  #rv <- xml_find_all(xml_thing, '//*[contains(concat( " ", @class, " " ), concat( " ", "arc-issue", " " ))]')
+  return(as_list(rv))
+
+}
+
+parse_main_page <- function(uri = "https://www.ncbi.nlm.nih.gov/pmc/journals/1440/" ) {
+
+  r <- GET(uri)
+
+  if (r$status_code != 200) {
+    stop(paste('failed to retrieve page: ', uri, '  status_code: ', r['status_code']))
+  }
+
+  rv <- content(r, type = 'text/html', encoding = 'UTF-8')
+  return(rv)
 }
 
 
--- project1/tests_main.R
+++ project1/tests_main.R
@@ -1,12 +1,10 @@
 source("./main.R")
 
 library(logging)
-#basicConfig(20)
-setLevel("DEBUG")
+basicConfig(level = 10)
+#setLevel("DEBUG")
 
 library(testthat)
-
-
 
 test_that("year < 1998 gets STOP", {
   expect_error(get_articles(start_year = 0))
@@ -17,42 +15,23 @@ test_that("year < 1998 gets STOP no parm ID", {
   expect_error(get_articles(0))
 })
 
+test_that("invalid page not 200", {
+  expect_error(parse_main_page('https://httpstat.us/404'))
+})
 
+test_that("test main page", {
+  r <- parse_main_page()
+  expect_equal(length(r), 2)
+})
 
+test_that("get all issues", {
+  s1 <- parse_main_page()
+  s2 <- find_all_issue(s1)
 
-# test_that("1 gets 1", {
-#   expect <- 1
-#   actual <- Fib(1)
-#   expect_equal(actual, expect)
-# })
-#
-# test_that("2 gets 1", {
-#   expect <- 1
-#   actual <- Fib(2)
-#   expect_equal(actual, expect)
-# })
-#
-# test_that("3 gets 2", {
-#   expect <- 2
-#   actual <- Fib(3)
-#   expect_equal(actual, expect)
-# })
-#
-# test_that("4 gets 3", {
-#   expect <- 3
-#   actual <- Fib(4)
-#   expect_equal(actual, expect)
-# })
-#
-# test_that("8 gets 21", {
-#   expect <- 21
-#   actual <- Fib(8)
-#   expect_equal(actual, expect)
-# })
-#
-# test_that("100 gets 354224848179261915075", {
-#   expect <- 354224848179261915075
-#   actual <- Fib(100)
-#   expect_equal(actual, expect)
-# })
+  expect_gte(length(s2), 49)
 
+  result <- evaluate_promise(parse_all_issues(s2), print = TRUE)
+  print(result$message)
+  print(result$output)
+
+})
